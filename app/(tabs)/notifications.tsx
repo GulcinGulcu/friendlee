955c2c2aa8c8302d3c18a1e5084534e68a547e80
gulcin <gulcin_375@hotmail.com>
2025-10-21 10:25:39 +0200

    feat: add tabs screens and initial setups

---
+++ app/(tabs)/notifications.tsx
@@ -0,0 +1,11 @@
+import { View, Text } from "react-native";
+
+const Notifications = () => {
+  return (
+    <View>
+      <Text>Noticifations</Text>
+    </View>
+  );
+};
+
+export default Notifications;
