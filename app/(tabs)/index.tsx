9132c6cd57cb0f959ee4a74fd96775a5cb568514
gulcin <gulcin_375@hotmail.com>
2025-11-11 13:38:06 +0100

    feat(index): render new Post and Story components

--- app/(tabs)/index.tsx
+++ app/(tabs)/index.tsx
@@ -1,20 +1,71 @@
+import Story from "@/components/Story";
+import { STORIES } from "@/constants/mock-data";
+import { COLORS } from "@/constants/theme";
+import { api } from "@/convex/_generated/api";
+import { styles } from "@/styles/feed.styles";
 import { useAuth } from "@clerk/clerk-expo";
-import { Link } from "expo-router";
-import { Text, TouchableOpacity, View } from "react-native";
+import { Ionicons } from "@expo/vector-icons";
+import { useQuery } from "convex/react";
+import Loader from "@/components/Loader";
+import Post from "@/components/Post";
+import { ScrollView, Text, TouchableOpacity, View } from "react-native";
 
 export default function Index() {
   const { signOut } = useAuth();
+
+  const posts = useQuery(api.posts.fetchPosts);
+
+  if (posts === undefined) return <Loader />;
+
+  if (posts.length === 0) return <NoPostsFound />;
+
+  return (
+    <View style={styles.container}>
+      <View style={styles.header}>
+        <Text style={styles.headerTitle}>Friendlee</Text>
+        <TouchableOpacity onPress={() => signOut()}>
+          <Ionicons name="log-out-outline" color={COLORS.white} size={22} />
+        </TouchableOpacity>
+      </View>
+      <ScrollView
+        showsVerticalScrollIndicator={false}
+        contentContainerStyle={{ paddingBottom: 60 }}
+      >
+        <ScrollView
+          showsHorizontalScrollIndicator={false}
+          horizontal
+          style={styles.storiesContainer}
+        >
+          {STORIES.map((story) => (
+            <Story key={story.id} story={story} />
+          ))}
+        </ScrollView>
+        {posts.map((post) => (
+          <Post key={post._id} post={post} />
+        ))}
+      </ScrollView>
+    </View>
+  );
+}
+
+const NoPostsFound = () => {
   return (
     <View
       style={{
         flex: 1,
+        backgroundColor: COLORS.background,
         justifyContent: "center",
         alignItems: "center",
       }}
     >
-      <TouchableOpacity onPress={() => signOut()}>
-        <Text>Sign Out</Text>
-      </TouchableOpacity>
+      <Text
+        style={{
+          color: COLORS.primary,
+          fontSize: 22,
+        }}
+      >
+        No posts yet
+      </Text>
     </View>
   );
-}
+};
